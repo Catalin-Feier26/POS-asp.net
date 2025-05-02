822fdfd5327cc2e92b15e6b90feaa5417cb481d4
Catalin <feiercatalin123@icloud.com>
2025-05-02 21:15:27 +0300

    Frontend for the Menu Management

---
+++ RestaurantPOS/Pages/Admin/ManageMenu.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RestaurantPOS.Pages.Admin;
+
+public class ManageMenu : PageModel
+{
+    public IActionResult OnGet()
+    {
+        var role = HttpContext.Session.GetString("role");
+        if (role != "Admin")
+        {
+            return RedirectToPage("/Auth/Login");
+        }
+        return Page();
+    }
+}
