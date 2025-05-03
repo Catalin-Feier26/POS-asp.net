49b45fe86f8963c6f5a9bbe35870eee4af4e0f9d
Catalin <feiercatalin123@icloud.com>
2025-05-03 18:38:45 +0300

    Frontend for the Table Management part

---
+++ RestaurantPOS/Pages/Server/ManageTables.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RestaurantPOS.Pages.Server;
+
+public class ManageTables : PageModel
+{
+    public IActionResult OnGet()
+    {
+        var role = HttpContext.Session.GetString("role");
+        if (role != "Server")
+        {
+            return RedirectToPage("/Auth/Login");
+        }
+        return Page();
+    }
+}
